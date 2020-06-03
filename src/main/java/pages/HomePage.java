2b8889ce629d5076dd8434370b2638d138760c7f
Oksana Gorbachenko <>
2020-06-03 14:31:19 +0300

    add fluent style

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import utils.WebDriverFactory;
 
-
 import static pages.PagesURLs.base;
 
 public class HomePage {
@@ -22,37 +21,48 @@ public class HomePage {
       By.xpath("//a[contains(@class, 'nav-link active ng-binding') and child::text()]");
   private By tagInArticleList = By.xpath("//ul[contains(@class,  'tag-list') and child::text()]");
 
-  public HomePage() {
+  public HomePage(WebDriver driver) {
     this.driver = WebDriverFactory.getDriver();
     commonActions = new CommonActions(driver);
   }
 
+  public static HomePage using(WebDriver driver) {
+    return new HomePage(driver);
+  }
+
   @Step("Open page: " + base)
-  public void navigate() {
+  public HomePage navigate() {
     commonActions.loggerPrint("Open page: " + base);
     driver.get(base);
+    return this;
   }
 
   @Step("Assert test tag is displayed")
-  public void assertTagInHeader() {
+  public HomePage assertTagInHeader() {
     commonActions.loggerPrint("Assert that #test tag is displayed");
     String filterNameInHeader = commonActions.find(currentHashTag).getText();
     Assert.assertEquals(filterNameInHeader, selectedFilterName);
+    return this;
   }
 
   @Step("Select filter by index")
-  public void selectFilterByIndex(Integer index) {
+  public HomePage selectFilterByIndex(Integer index) {
     selectedFilterName =
         commonActions.find(hashTagList).findElements(hashTagList).get(index).getText();
     commonActions.find(hashTagList).findElements(hashTagList).get(index).click();
+    return this;
   }
 
   @Step
-  public void assertTagInArticle() {
-    Object[] tagInArticleListArray = commonActions.find(tagInArticleList).findElements(tagInArticleList).toArray();
+  public HomePage assertTagInArticle() {
+    Object[] tagInArticleListArray =
+        commonActions.find(tagInArticleList).findElements(tagInArticleList).toArray();
     for (int i = 0; i < tagInArticleListArray.length; i++) {
-      String tag = commonActions.find(tagInArticleList).findElements(tagInArticleList).get(i).getText();
+      String tag =
+          commonActions.find(tagInArticleList).findElements(tagInArticleList).get(i).getText();
       Assert.assertTrue(tag.contains(selectedFilterName));
+      System.out.println(tag);
     }
+    return this;
   }
 }
--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -1,15 +1,16 @@
 import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import utils.WebDriverFactory;
 
 public class FilterTest extends BaseTest {
   @Description("Test filters with hashTag")
   @Test()
   public void filtersTest() {
-    HomePage homePage = new HomePage();
-    homePage.navigate();
-    homePage.selectFilterByIndex(0);
-    homePage.assertTagInHeader();
-    homePage.assertTagInArticle();
+    HomePage.using(WebDriverFactory.getDriver())
+    .navigate()
+    .selectFilterByIndex(0)
+    .assertTagInHeader()
+    .assertTagInArticle();
   }
 }
